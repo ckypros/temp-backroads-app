453b308fc9732d9c9ccd17491dafc404e01387b4
Charles Kypros <Ckypros@gmail.com>
2023-05-09 00:30:01 -0500

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import logoImg from '../images/logo.svg';
+import { socialLinks } from '../data';
+import PageLinks from './PageLinks';
+import SocialLink from './SocialLink';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logoImg} className='nav-logo' alt='backroads' />
+          <button type='button' className='nav-toggle' id='nav-toggle'>
+            <i cla ssName='fas fa-bars'></i>
+          </button>
+        </div>
+        <PageLinks parentClass='nav-links' itemClass='nav-link' />
+
+        <ul className='nav-icons'>
+          {socialLinks.map((link) => {
+            return <SocialLink {...link} key={link.id} itemClass='nav-icon' />;
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
